a75e5434dc0e96ee4dd8ccbaf00134200e3ad299
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-03 01:40:11 +0530

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  styleNo: String,
+  price: Number,
+  stock: Number,
+
+  images: [String],
+  primaryImage: String,
+
+  brand: String,
+  fabric: String,
+  typeDetail: String,
+  fit: String,
+  pattern: String,
+  color: String,
+  occasion: String,
+  description: String,
+
+  sizes: [String],
+
+  sizeStock: [{
+    size: String,
+    stock: Number
+  }],
+
+  category: String
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
