d6afd623f827a60c8838faa2f4d19b8ec2a94390
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-09 05:02:57 +0530

    Update sync.js

--- sync.js
+++ sync.js
@@ -100,7 +100,7 @@ catch(err){
 
         console.log("429...Waiting 10 seconds");
 
-        await new Promise(resolve=>setTimeout(resolve,10000));
+        await new Promise(resolve=>setTimeout(resolve,30000));
 
         continue;
 
