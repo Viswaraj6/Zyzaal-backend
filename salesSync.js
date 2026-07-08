b9da466340b14bb7ba0401e54dd488cd56b45c02
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-09 00:30:29 +0530

    Update salesSync.js

--- salesSync.js
+++ salesSync.js
@@ -2,12 +2,14 @@ const axios = require("axios");
 const { getAccessToken } = require("./zoho");
 
 async function syncSales() {
+
     try {
 
         const token = await getAccessToken();
 
+        // 1. Get latest invoices
         const res = await axios.get(
-             `https://www.zohoapis.in/inventory/v1/invoices/${invoice.invoice_id}`,
+            "https://www.zohoapis.in/inventory/v1/invoices",
             {
                 params: {
                     organization_id: process.env.ZOHO_ORGANIZATION_ID,
@@ -19,7 +21,25 @@ async function syncSales() {
             }
         );
 
-        console.log(JSON.stringify(res.data, null, 2));
+        // 2. First invoice
+        const invoice = res.data.invoices[0];
+
+        console.log("Invoice ID:", invoice.invoice_id);
+
+        // 3. Get invoice details
+        const detail = await axios.get(
+            `https://www.zohoapis.in/inventory/v1/invoices/${invoice.invoice_id}`,
+            {
+                params: {
+                    organization_id: process.env.ZOHO_ORGANIZATION_ID
+                },
+                headers: {
+                    Authorization: `Zoho-oauthtoken ${token}`
+                }
+            }
+        );
+
+        console.log(JSON.stringify(detail.data, null, 2));
 
     } catch (err) {
 
