9eea2d7d74d9b87c16d8522ae9ca20fccab6ae2e
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-27 16:11:38 +0530

    Update salesSync.js

--- salesSync.js
+++ salesSync.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const { getAccessToken } = require("./zoho");
 const SyncStatus = require("./models/SyncStatus");
-
+const { updateExcel } = require("./excelHelper");
 function log(msg){
 
     console.log(msg);
